8dfd27551fcd4025cda640e3b4ee7114fb0f315d
Austin <martenaustin@rocketmail.com>
2021-10-03 11:49:59 -0500

    new file:   indexphp.php

---
+++ indexphp.php
@@ -0,0 +1,19 @@
+<?php
+
+print "<!DOCTYPE html>\n";
+print "<html lang='en'>\n";
+print "    <head>\n";
+print "        <!--Only added title for Validation-->\n";
+print "     <title>Title</title>\n";
+print "     <meta charset=\"utf-8\">\n";
+print "     <link rel=\"stylesheet\" href=\"style.css\">\n";
+print "     <script src=\"hello.js\"></script>   \n";
+print "</head>\n";
+print "<body>\n";
+print "\n";
+print "<h1>Austin Marten</h1>\n";
+print "\n";
+print "</body>\n";
+print "</html>\n";
+
+?>
